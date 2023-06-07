c3c27031480a8eab6f317d27cb7067e0cf9a183c
Karlisson Ferreira <kbb25t@gmail.com>
2023-06-06 23:21:52 -0300

    creation of the 'form' /and use of validators

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,19 +1,21 @@
-import React,{useState} from "react";
+import React, {useState} from "react";
 import { Container, Form } from "./styles";
 import Input from "../../Components/Input";
 import Button from "../../Components/Button";
+import { validateEmail, validatePassword } from '../../Utils/validators';
 
 
 const Login = () => {
     //state loading vai manipular as requisiçoes 
     const [loading, setLoading] = useState()
+    const [form, setForm] = useState([])
 
     //função feita para que a API busque as informaçoes no backend e logo apos continue executando 
-    const handleSubmit = async (event)=>{ 
+    const handleSubmit = async (event) =>{ 
         event.preventDefault();
         try{
             setLoading(true)
-            alert('Login')
+            alert('Sucesso')
             setLoading(false)
         }
         catch(err) {
@@ -25,7 +27,17 @@ const Login = () => {
     //captura tanto o nome quanto o tipo do input
     const handleChange = (event) =>{
         console.log('Digitando...', event.target.name, event.target.value)
+    // quando o 'handleChange' for acionado vai ativar o 'spread' e assim vai mudar o valor do 'form'
+        setForm({...form, [event.target.name]: event.target.value})
+        console.log('Form', form)
     }
+
+
+    const inputValidator = () =>{
+        return validateEmail(form.email) && validatePassword(form.password)
+    }
+
+
 
     return (
        <Container>
@@ -47,8 +59,9 @@ const Login = () => {
             />
         <Button
             type='submit'
-            text='Entrar!'
+            text='Entrar'
             onClick={handleSubmit}
+            disable={loading === true || !inputValidator()}
         />
     
             <div>
