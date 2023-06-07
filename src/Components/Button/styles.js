042b84d640bc02b770e91834e52e45c7efd773ef
Karlisson Ferreira <kbb25t@gmail.com>
2023-06-06 23:20:27 -0300

    button animation

--- src/Components/Button/styles.js
+++ src/Components/Button/styles.js
@@ -12,4 +12,15 @@ color: #FFF;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   cursor: pointer;
   
+  
+  ${props => props.disabled === false &&`
+    &:hover {
+      background-color: #FFF;
+      color: #6a6a6a;
+    }
+  `}
+
+  ${props => props.disabled &&`
+    opacity: 0.7
+  `}
 `
