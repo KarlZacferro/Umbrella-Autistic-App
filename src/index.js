fb58be893e0f6e25dca4b894ec412a3f77ff3075
Karlisson Ferreira <kbb25t@gmail.com>
2023-06-20 22:47:00 -0300

    import functiom

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import Login from "./Pages/Login";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    
     <React.StrictMode>
         <Login/>
     </React.StrictMode>
