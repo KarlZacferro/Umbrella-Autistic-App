0b5e823f521671c2d50bca5250aeeca086c00674
Karlisson Ferreira <kbb25t@gmail.com>
2023-06-25 20:41:58 -0300

    add string

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Login from "./Pages/Login";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    
     <React.StrictMode>
         <Login/>
     </React.StrictMode>
